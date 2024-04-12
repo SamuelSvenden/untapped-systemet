2aeadbd2526b1f78921615ca7e678e89f5169a0d
Samuel Svendén <samuel.svenden.1664@student.uu.se>
2024-04-12 16:23:56 +0200

    added apk calc

--- service-worker.js
+++ service-worker.js
@@ -28,8 +28,44 @@ function addButtonToElement(element) {
       link.appendChild(button);
       element.appendChild(link);
     });
-  }, 500);
+  }, 50);
 }
+
+
+function addApkToElement(element) {
+  setTimeout(() => {
+    const elements = document.querySelectorAll('.css-6df2t1');
+    console.log(elements.length);
+
+    elements.forEach((element) => {
+
+      const p = document.createElement("p");
+      p.innerText = "APK: ";
+
+      
+      var element1 = element.querySelectorAll('.css-bbhn7t');
+      const innerHtmls = Array.from(element1).map(el => el.innerHTML);
+      var price = element.querySelector('.css-17znts1')
+
+
+      var element2 = innerHtmls[1] ? parseFloat(innerHtmls[1].replace(',', '.')) : 0;
+      var element3 = innerHtmls[2] ? parseFloat(innerHtmls[2].replace(',', '.')) : 0;
+      var element4 = price ? parseFloat(price.innerText.replace(':', '.').replace('*', '')) : 0;
+
+
+      var apk = (element2*(element3/100))/element4;
+      
+
+
+    
+      p.append(apk.toFixed(2));
+      element.appendChild(p);
+    });
+  }, 50);
+}
+
+
+
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'complete' && tab && tab.url && !tab.url.includes('chrome://')) {
@@ -39,4 +75,16 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     });
   }
 });
+
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete' && tab && tab.url && !tab.url.includes('chrome://')) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: addApkToElement
+    });
+  }
+});
+
+
 
